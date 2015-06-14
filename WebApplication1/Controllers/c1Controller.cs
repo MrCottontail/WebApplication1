2699eaedab0f1cbe97fd92d5f12110c499ae39dc
MrCottontail <pctrker@gmail.com>
2015-06-14 15:00:35 +0300

    My Project

---
+++ WebApplication1/Controllers/c1Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class c1Controller : Controller
+    {
+        // 
+        // GET: /c1/ 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /c1/Welcome/ 
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
